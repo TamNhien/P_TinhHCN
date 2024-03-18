1d55a0628f1955ec1c4ddb86febb3f20a937e101
TamNhien <canhthien91@gmail.com>
2024-03-18 20:41:47 +0700

    Add project files.

---
+++ P_TinhHCN.py
@@ -0,0 +1,13 @@
+﻿def main():
+    a = float(input("Nhập chiều dài của hình chữ nhật : "))
+    b = float(input("Nhập chiều rộng của hình chữ nhật : "))
+
+    P = 2 * (a + b)
+    S = a * b
+
+    print(f"Chu vi của hình chữ nhật là : {P}")
+    print(f"Diện tích của hình chữ nhật là : {S}")
+
+if __name__ == "__main__":
+    main()
+
